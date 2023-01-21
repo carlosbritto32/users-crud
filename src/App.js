190cdc4a2a9926f2d5f08a24d9a90d1984d03ac4
CarlosBritto32 <carlosbritto32@gmail.com>
2023-01-21 16:03:52 -0500

    Feat: create the component pages, then use browser router to manage the routes, then create the context to comunicate tha state between the pages and components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import { Routes, Route } from "react-router-dom";
+import { Dashboard } from "./pages/Dashboard";
+import { Login } from "./pages/Login";
+import { Register } from "./pages/Register";
+import { ProtectedRoutes } from "./components/ProtectedRoutes";
+import { AuthProvider } from "./context/authContext";
 import "./App.css";
 
 function App() {
-  return <div className="App">hello world</div>;
+  return (
+    <div>
+      <AuthProvider>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <ProtectedRoutes>
+                <Dashboard />
+              </ProtectedRoutes>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </AuthProvider>
+    </div>
+  );
 }
 
 export default App;
